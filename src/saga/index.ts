6a23a687bc9777dea6073ac7da8fac92a0c20d93
Jed Dylan Lee <jeddylanlee@gmail.com>
2026-02-07 20:06:44 +0800

    Setup root saga / saga index in index file; Added comments for each line

---
+++ src/saga/index.ts
@@ -0,0 +1,26 @@
+/**
+ * @all - runs multiple sagas in parallel and waits for all of them to complete
+ * @fork - creates a non-blocking call to a saga, allowing the parent saga to continue executing without waiting for the forked saga to finish
+ */
+import { all, fork } from "redux-saga/effects";
+
+/**
+ * @function* - the asterisk makes this a generator function (required for sagas)
+ * @rootSaga - the main saga that combines all other sagas in the application;
+ * It is the entry point for all side effects in the app; Exported to be run by the saga middleware in the store configuration file
+ * @yield - pauses execution and waits for the effect to complete
+ * @all - runs all sagas inside the array simultaneously
+ */
+export function* rootSaga() {
+  yield all(
+    [
+      /**
+       * Add your sagas here
+       * example:
+       * fork(userSaga),
+       * fork(productsSaga),
+       * fork(cartSaga),
+       */
+    ].map(fork),
+  );
+}
