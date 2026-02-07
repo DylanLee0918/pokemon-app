67fbac871dc0d2ef89bace5ff0148cff3b5aaee1
Jed Dylan Lee <jeddylanlee@gmail.com>
2026-02-07 20:08:37 +0800

    Imported Provider from react-redux; Wrapped the app with Provider then pass store as props in provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store.ts";
 import "./styles/index.css";
 
 function App() {
   return (
     <React.Fragment>
-      <div>This is a new react app.</div>
+      <Provider store={store}>
+        <div>This is a new react app.</div>
+      </Provider>
     </React.Fragment>
   );
 }
