31bcdb94ab3f46baeeae2b537ae5fbba23396efc
Jed Dylan Lee <jeddylanlee@gmail.com>
2026-02-08 20:31:29 +0800

    Migrate store with redux/toolkit instead of old approach

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,4 @@
-import { applyMiddleware, legacy_createStore as createStore } from "redux";
-
-import { composeWithDevTools } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 
 /** Imports combined reducers */
@@ -10,28 +8,30 @@ import { rootSaga } from "./saga";
 
 /** Creates middleware that allows redux to work with sagas */
 const sagaMiddleware = createSagaMiddleware();
-/** An array of middleware to apply, but can add other middleware */
-const middleware = [sagaMiddleware];
-/** Starting state of redux store; reducers will use their own default state */
-const initialState = {};
 
 /**
- * @reducers - state management logic for the app
- * @initialState - the starting state of the app
- * @middleware - an array of middleware to apply to the store, but can add other middleware
- * @composeWithDevTools - a function that enhances the store with dev tools capabilities
+ * @reducer - state management logic for the app
+ * @middleware - configures saga middleware
+ * @devTools - automatically includes Redux DevTools in development
  */
-const store = createStore(
-  reducers,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware)),
-);
+const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: false, // We're using sagas, not thunks
+      serializableCheck: false, // Adjust based on your needs
+    }).concat(sagaMiddleware),
+});
 
 /**
  * Starts the saga middleware; making it actively listen for actions
  * and execute the corresponding sagas when those actions are dispatched.
  */
 sagaMiddleware.run(rootSaga);
+
+/** Export types for TypeScript */
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 /** Exports the configured store so you can provide it to React App via <Provider store={store}> */
 export default store;
