750eed8910c820769ac14d848c9652caf15393e4
Jed Dylan Lee <jeddylanlee@gmail.com>
2026-02-08 15:09:46 +0800

    Created new component MainPage

---
+++ src/pages/Main/MainPage.tsx
@@ -0,0 +1,12 @@
+import type { FunctionComponent } from "react";
+import Pokemon from "../../features/Pokemon";
+
+const MainPage: FunctionComponent = () => {
+  return (
+    <>
+      <Pokemon />
+    </>
+  );
+};
+
+export default MainPage;
